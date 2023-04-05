d1de9c29e568b61abd5127e4fa9cbd6c6c1de173
James park <dayung0425@gmail.com>
2023-04-05 17:13:41 +0900

    classloder

---
+++ java/src/com/example/Bus.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Bus extends Car {
+    public void a(){
+        System.out.println("a");
+    }
+    public void b(){
+        System.out.println("b");
+    }
+    public void c(){
+        System.out.println("c");
+    }
+
+}
