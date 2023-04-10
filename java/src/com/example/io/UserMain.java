0f7becba88b59c241135eb0a31aababf7b19a6de
James park <dayung0425@gmail.com>
2023-04-10 14:20:06 +0900

    이메일 수정하기

--- java/src/com/example/io/UserMain.java
+++ java/src/com/example/io/UserMain.java
@@ -16,11 +16,29 @@ public class UserMain {
                 userDao.saveUser(users);
                 break;
             } else if (menuId == 1) {
-                User user = userUI.regMenu();
+                User user = userUI.regUser();
                 users.add(user);
                 System.out.println("등록되었습니다");
             } else if(menuId == 2){
                 userUI.printUserList(users);
+            } else if (menuId == 3) {
+                String email = userUI.inputEmail();
+                int findIndex = -1;
+                for(int i=0; i<users.size(); i++){
+                    User u = users.get(i);
+                    if(u.getEmail().equals(email)){
+                        findIndex = i;
+                        break;
+                    }
+                }
+                if(findIndex != -1) {
+                    User updateUser = userUI.inputUser(email);
+                    users.remove(findIndex);
+                    users.add(updateUser);
+                    System.out.println("수정되었습니다");
+                }else{ // 찾지 못했다.
+                    System.out.println("수정할 회원 정보가 없습니다");
+                }
             }
         }
     }
--- java/src/com/example/io/UserUI.java
+++ java/src/com/example/io/UserUI.java
@@ -13,6 +13,7 @@ public class UserUI {
     public int menu(){
         System.out.println("1. 회원 등록");
         System.out.println("2. 회원 목록 보기");
+        System.out.println("3. 회원 정보 수정하기");
         System.out.println("5. 종료");
 
         int menuId = -1;
@@ -25,7 +26,36 @@ public class UserUI {
         return menuId;
     }
 
-    public User regMenu(){
+    public String inputEmail(){
+        System.out.println("수정할 회원의 email을 입력해주세요");
+        String email = "";
+        try {
+            email = br.readLine();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return email;
+    }
+
+    public User inputUser(String email){
+        try{
+            System.out.println(email + "회원 정보를 수정합니다.");
+            System.out.println("이름을 입력하세요");
+            String name = br.readLine();
+            System.out.println("생년을 입력하세요");
+            String strBirthYear = br.readLine();
+            int birthYear = Integer.parseInt(strBirthYear);
+
+            User user = new User(email, name, birthYear);
+            return user;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+
+        }
+    }
+
+    public User regUser(){
         try{
             System.out.println("email을 입력하세요");
             String email = br.readLine();
